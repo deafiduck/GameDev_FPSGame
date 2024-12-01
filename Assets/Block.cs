6f5f0876edd09ea0ee79bbf7e87850175c53f077
aley22 <aleyna.izgi.2oo2@gmail.com>
2024-12-02 02:27:11 +0300

    sadece collider eklendi o da bazı yerlere

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -4,53 +4,66 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField] int BlockHealth = 100;
+	[SerializeField] int BlockHealth = 100;
 
-    [SerializeField] GameObject GameObjectHide;
-    [SerializeField] GameObject GameObjectShow;
-    [SerializeField] List<GameObject> Zombies;
+	[SerializeField] GameObject GameObjectHide;
+	[SerializeField] GameObject GameObjectShow;
+	[SerializeField] List<GameObject> Zombies;
+	[SerializeField] AudioClip interactionSound; // Ses dosyasýný tanýmlayýn
 
-    private void Update()
-    {
-        if (BlockHealth < 0)
-        {
-            BlockHealth = 0;
-        }
-        
-        
-    }
+	private AudioSource audioSource;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
+	private void Start()
+	{
+		audioSource = gameObject.AddComponent<AudioSource>(); // AudioSource bileþeni ekleyin
+	}
 
-            GameObjectShow.SetActive(true);
-            GameObjectHide.SetActive(false);
-            if (Zombies != null && Zombies.Count > 0)
-            {
-                foreach (GameObject zombie in Zombies)
-                {
-                    if (zombie != null)
-                    {
-                        zombie.SetActive(true);  // Her bir zombiyi aktif hale getir
-                    }
-                }
-            }
-        }
-    }
-    public void ReduceHealth(int reduceHealth)
-    {
-        BlockHealth -= reduceHealth;
-        Debug.Log("block Health Reduced: " + BlockHealth);
+	private void Update()
+	{
+		if (BlockHealth < 0)
+		{
+			BlockHealth = 0;
+		}
+	}
 
-        if (BlockHealth <= 0)
-        {
-            BlockHealth = 0;
-            gameObject.SetActive(false);
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			PlayInteractionSound(); // Ses efektini çal
+			GameObjectShow.SetActive(true);
+			GameObjectHide.SetActive(false);
+			if (Zombies != null && Zombies.Count > 0)
+			{
+				foreach (GameObject zombie in Zombies)
+				{
+					if (zombie != null)
+					{
+						zombie.SetActive(true); // Her bir zombiyi aktif hale getir
+					}
+				}
+			}
+		}
+	}
 
-        }
-    }
+	public void ReduceHealth(int reduceHealth)
+	{
+		BlockHealth -= reduceHealth;
+		Debug.Log("Block Health Reduced: " + BlockHealth);
 
+		if (BlockHealth <= 0)
+		{
+			BlockHealth = 0;
+			gameObject.SetActive(false);
+		}
+	}
 
+	private void PlayInteractionSound()
+	{
+		if (interactionSound != null)
+		{
+			audioSource.clip = interactionSound;
+			audioSource.Play();
+		}
+	}
 }
